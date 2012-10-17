22126be154604edcb2aec10d8e0166e7afefe752
Sylvan Knave <gc1812@yahoo.com>
2012-10-16 22:25:58 -0500

    skeleton pages for the front-end

---
+++ public/javascripts/initialize.js
@@ -0,0 +1,12 @@
+$( document ).bind( "mobileinit", function() {
+    // Make your jQuery Mobile framework configuration changes here!
+  $.support.cors = true;
+  $.mobile.allowCrossDomainPages = true;
+  $.mobile.fallbackTransition.slideout = "none";
+  
+  // otherwise linking to other pages don't flow well
+  $.mobile.ajaxEnabled = false;
+
+  //defaults dates to text after selection
+  $.mobile.page.prototype.options.degradeInputs.date = true;
+});
